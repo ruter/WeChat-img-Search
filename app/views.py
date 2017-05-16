ea06b3cbaead2c37d6a68311deb5b81bcd670621
Ruter <ruter.lv@gmail.com>
2017-05-16 23:27:15 +0800

    Add verification

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, redirect, url_for
+import hashlib
+from flask import abort
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import render_template
 
 from . import app, collection
+from . import receive
 
 
 @app.errorhandler(404)
@@ -15,6 +21,28 @@ def internal_error(error):
     return 500
 
 
-@app.route('/')
-def index():
-    return 'hello, world'
+@app.route('/', methods=['GET', 'POST'])
+def wechat():
+    if request.method == 'GET':
+        # Verify
+        try:
+            signature = request.args['signature']
+            timestamp = request.args['timestamp']
+            nonce = request.args['nonce']
+            echostr = request.args['echostr']
+
+            temp_list = [app.config['TOKEN'], timestamp, nonce]
+            temp_list.sort()
+
+            sha1 = hashlib.sha1()
+            map(sha1.update, temp_list)
+            hash_code = sha1.hexdigest()
+
+            if hash_code == signature:
+                return echostr
+            else:
+                abort(404)
+        except Exception as e:
+            return e
+    else:
+        return 'hello, world'
