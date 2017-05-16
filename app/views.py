139fe84582de82564e7f7b558a228ca8e2ae593d
Ruter <ruter.lv@gmail.com>
2017-05-16 21:53:47 +0800

    Fixed error

--- app/views.py
+++ app/views.py
@@ -10,11 +10,11 @@ def not_found_error(error):
     return 404
 
 
-@app.errorhandler(500):
+@app.errorhandler(500)
 def internal_error(error):
     return 500
 
 
 @app.route('/')
 def index():
-    pass
+    return 'hello, world'
